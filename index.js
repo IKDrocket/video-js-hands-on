5016e5620ce04c1a52efa3376104eda7686cd999
IKDrocket <yusuke1128rocket@gmail.com>
2022-08-25 19:53:40 +0900

    remove some option

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const player = videojs("my-video", {
+  poster: "https://peach.blender.org/wp-content/uploads/title_anouncement.jpg",
   // 動画のアスペクト比を自動で調整する
   fluid: true,
   // コントロールバーを表示にする
@@ -20,34 +21,34 @@ const player = videojs("my-video", {
   controlBar: {
     volumePanel: { inline: false },
   },
-
-  // tracks: [
-  //   {
-  //     src: "./captions/jp.vtt",
-  //     kind: "captions",
-  //     srclang: "jp",
-  //     label: "Japanese",
-  //   },
-  //   {
-  //     src: "./captions/en.vtt",
-  //     kind: "captions",
-  //     srclang: "en",
-  //     label: "English",
-  //   },
-  // ],
+  // 字幕の設定
+  tracks: [
+    {
+      src: "./captions/jp.vtt",
+      kind: "captions",
+      srclang: "jp",
+      label: "Japanese",
+    },
+    {
+      src: "./captions/en.vtt",
+      kind: "captions",
+      srclang: "en",
+      label: "English",
+    },
+  ],
 });
 
 player.src("https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8");
-player.hlsQualitySelector({ displayCurrentQuality: true });
-player.vttThumbnails({ src: videoMetaData[0].thumbnails, showTimestamp: true });
 
-videoMetaData[0].metaData.tracks.forEach((track) => {
-  player.addRemoteTextTrack(track);
+// 画質選択の設定を有効にする
+player.hlsQualitySelector();
+
+// シークバーにマウスを乗せたときにサムネイルを表示する
+player.vttThumbnails({
+  src: "./thumbnails/big_buck_bunny_thumbnails.vtt",
 });
 
-const videolist = videoMetaData.map((video) => video.metaData);
-player.playlist(videolist);
-
+// 再生時間が更新されるたびに実行される関数を設定する
 player.on("timeupdate", function () {
   const span = document.getElementById("currentTime");
   span.innerText = player.currentTime();
@@ -62,39 +63,3 @@ const mute = () => player.muted(true);
 const unMute = () => player.muted(false);
 
 const playback = (x) => player.playbackRate(x);
-
-// const updateCaptions = (index) => {
-//   // if (index !== undefined) {
-//   const oldTracks = player.remoteTextTracks();
-//   for (let i = 0; i < oldTracks.length; i++) {
-//     player.removeRemoteTextTrack(oldTracks[i]);
-//   }
-//   // }
-//   videoMetaData[index].metaData.tracks.forEach((track) => {
-//     player.addRemoteTextTrack(track);
-//   });
-// };
-
-const previousVideo = () => {
-  const currentNumber = player.playlist.currentItem();
-  const previousNumber = currentNumber - 1;
-  // 前の動画が存在する場合
-  if (currentNumber !== 0) {
-    player.vttThumbnails.src(videoMetaData[previousNumber].thumbnails);
-    // updateCaptions(previousNumber);
-  }
-
-  player.playlist.previous();
-};
-
-const nextVideo = () => {
-  const currentNumber = player.playlist.currentItem();
-  const nextNumber = currentNumber + 1;
-  // 次の動画が存在する場合
-  if (nextNumber !== player.playlist().length) {
-    player.vttThumbnails.src(videoMetaData[nextNumber].thumbnails);
-    // updateCaptions(nextNumber);
-  }
-
-  player.playlist.next();
-};
